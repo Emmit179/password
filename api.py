0fc016d83e1677e180a91c367e2b36a66a960fc6
Emmit179 <68614251+Emmit179@users.noreply.github.com>
2023-01-02 21:02:11 -0600

    Update api.py

--- api.py
+++ api.py
@@ -113,12 +113,12 @@ def create_user():
     #     return jsonify({'message: ': 'cannot perform that function'})
 
 
-    name = db.users.find_one(name=data['name'])
+    name = db.users.find({},{"name":data['name']}).first()
 
     if name:
         return jsonify({"message: ": "username taken"})
 
-    email = db.users.find_one(name=data['email'])
+    email = db.users.find_one({},{"name":data['email']}).first()
 
     if email:
         return jsonify({"message: ": "email taken"})
